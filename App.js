a98a4c27a44a0d24245c2f237605b7916387583d
Andréa J S França <dejandrea@gmail.com>
2022-06-11 15:36:55 -0300

    Aula 63 Completa

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'
+import { Text, View, StyleSheet, TextInput,TouchableOpacity } from 'react-native';
+import { Header } from 'react-native-elements';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      text: "",
+      displayText:""
+    }
+  }
+  render() {
+    return (
+      <SafeAreaProvider>
+        <View style={styles.container}>
+          <Header
+            backgroundColor='#9c8210' centerComponent={{ text: 'Cute Little Monkey', style: { color: '#fff', fontSize: 20 } }}
+          />
+          <TextInput
+            style={styles.inputBox}
+            onChangeText={text => { this.setState({ text: text }) }}
+            value={this.state.text}
+          />
+          <TouchableOpacity 
+            style={styles.goButton}
+            onPress={()=>{this.setState({displayText:this.state.text})}}
+          >
+            <Text style={styles.buttonText}>IR</Text>
+          </TouchableOpacity>
+          <Text style={styles.displayText}>{this.state.displayText}</Text>
+        </View>
+      </SafeAreaProvider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  inputBox:{
+    marginTop:200,
+    width:"80%",
+    alignSelf:'center',
+    height:40,
+    textAlign:'center',
+    borderWidth:4,
+  },
+  goButton:{
+    width:'50%',
+    height:55,
+    alignSelf:'center',
+    padding:10,
+    margin:10
+  },
+  buttonText:{
+    textAlign:'center',
+    fontSize:30,
+    fontWeight:'bold'
+  },
+  displayText:{
+    textAlign:'center',
+    fontSize:30
+  }
 });
---
+++ README.md
@@ -0,0 +1,5 @@
+Instalações necessárias:
+
+Aula 63
+
+npm install react-native-elements
